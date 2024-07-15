7f1b8a6709deb5f476ac962735e4b4d397421709
gjr <wanqing20212021@outlook.com>
2024-07-15 22:42:03 +0800

    更改为客户端可持续输入ID

--- src/main/java/com/ahua/myRPC_01/server/RPCServer.java
+++ src/main/java/com/ahua/myRPC_01/server/RPCServer.java
@@ -1,13 +1,14 @@
-package com.ahua.myRPC_00.server;
+package com.ahua.myRPC_01.server;
 
 
 import com.ahua.myRPC_00.common.User;
 
-import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @author 86188
@@ -15,13 +16,15 @@ import java.net.Socket;
 public class RPCServer  {
     public static void main(String[] args){
         UserServiceImpl userService=new UserServiceImpl();
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
         try {
             ServerSocket serverSocket=new ServerSocket(8899);
             System.out.println("服务端开启");
+
             while(true){
                 Socket socket = serverSocket.accept();
                 System.out.println("连接成功");
-                new Thread(()->{
+                executorService.execute(()->{
                     try {
                         ObjectInputStream input=new ObjectInputStream(socket.getInputStream());
                         Integer id=(Integer) input.readObject();
@@ -37,7 +40,7 @@ public class RPCServer  {
                     }catch (Exception e){
                         e.printStackTrace();
                     }
-                }).start();
+                });
             }
         }catch (Exception e){
             e.printStackTrace();
